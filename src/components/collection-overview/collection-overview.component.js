882c3a847c2ade2e97c95172f6508ce62e811988
Alexander Gordon <62938794+alexgordontlv@users.noreply.github.com>
2020-11-11 14:02:05 +0200

    shop directories

---
+++ src/components/collection-overview/collection-overview.component.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import CollectionPreview from '../preview-collection/collection.component';
+import {selectShopCollections} from '../../redux/shop/shop.selectors';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+import './collections-overview.styles.scss';
+
+const CollectionOverview = ({collections}) =>{
+    return (
+        <div>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+            <CollectionPreview key={id} {...otherCollectionProps} />
+          ))}
+        </div>
+    )
+}
+const mapStateToProps = createStructuredSelector({
+    collections: selectShopCollections
+  })
+
+export default connect(mapStateToProps)(CollectionOverview);
--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,29 +1,17 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/preview-collection/collection.component';
+import CollectionOverview from '../../components/collection-overview/collection-overview.component';
+import {Route} from 'react-router-dom';
+import CollectionPage from '../collection/collection.component';
 
-
-class ShopPage extends React.Component{
-    constructor(props){
-        super(props)
-
-        this.state = {
-            collections: SHOP_DATA
-        } 
-    }
-    render() {
-        const { collections } = this.state;
+const ShopPage = ({match}) => {
+     
         return (
           <div className='shop-page'>
-            {collections.map(({ id, ...otherCollectionProps }) => (
-              <CollectionPreview key={id} {...otherCollectionProps} />
-            ))}
+            <Route exact path={`${match.path}`} component={CollectionOverview}/>
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
           </div>
         );
       }
-
-}
-
 
 
 export default ShopPage;
